e1c48d1d983ca3a69169a05f8ca28ded12485074
David Castro <phoenix6ccxr@gmail.com>
2025-03-15 05:31:01 -0300

    deploy_proyectoFinalBckFnt

--- frontEnd/src/context/ProductContext.jsx
+++ frontEnd/src/context/ProductContext.jsx
@@ -1,7 +1,8 @@
 import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/productos/"; // URL correcta del backend
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 
 export const ProductContext = createContext();
 
